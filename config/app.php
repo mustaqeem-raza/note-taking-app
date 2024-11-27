12e832689c9979992ec9d3815583a650589e65ad
Mustaqeem Raza <77394916+mustaqeem-raza@users.noreply.github.com>
2024-11-27 21:59:32 +0500

    fixes

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Ladumor\LaravelPwa\PWAServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -209,6 +210,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- PWA  -->
+        <meta name="theme-color" content="#6777ef"/>
+        <link rel="apple-touch-icon" href="{{ asset('publisher.jpg') }}">
+        <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -128,5 +133,22 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('/sw.js') }}"></script>
+        <script>
+            if ("serviceWorker" in navigator) {
+                // Register a service worker hosted at the root of the
+                // site using the default scope.
+                navigator.serviceWorker.register("/sw.js").then(
+                (registration) => {
+                    console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                    console.error(`Service worker registration failed: ${error}`);
+                },
+                );
+            } else {
+                console.error("Service workers are not supported.");
+            }
+        </script>
     </body>
 </html>
